7ad91aed3ec4e62ad66086536e665834e44403ed
lumichy <lumichy@gmail.com>
2025-07-20 14:09:32 +0900

    初回の実装: Converse APIを使用して画像とテキストプロンプトを送信する機能を追加

---
+++ converseAPIImg.py
@@ -0,0 +1,113 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+"""
+Shows how to send an image with the <noloc>Converse</noloc> API with an accompanying text prompt to Anthropic Claude 3 Sonnet (on demand).
+"""
+
+import logging
+import boto3
+
+
+from botocore.exceptions import ClientError
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def generate_conversation(bedrock_client,
+                          model_id,
+                          input_text,
+                          input_image):
+    """
+    Sends a message to a model.
+    Args:
+        bedrock_client: The Boto3 Bedrock runtime client.
+        model_id (str): The model ID to use.
+        input text : The text prompt accompanying the image.
+        input_image : The path to the input image.
+
+    Returns:
+        response (JSON): The conversation that the model generated.
+
+    """
+
+    logger.info("Generating message with model %s", model_id)
+
+    # Get image extension and read in image as bytes
+    image_ext = input_image.split(".")[-1]
+    with open(input_image, "rb") as f:
+        image = f.read()
+
+    message = {
+        "role": "user",
+        "content": [
+            {
+                "text": input_text
+            },
+            {
+                "image": {
+                    "format": image_ext,
+                    "source": {
+                        "bytes": image
+                    }
+                }
+            }
+        ]
+    }
+
+    messages = [message]
+
+    # Send the message.
+    response = bedrock_client.converse(
+        modelId=model_id,
+        messages=messages
+    )
+
+    return response
+
+
+def main():
+    """
+    Entrypoint for Anthropic Claude 3 Sonnet example.
+    """
+
+    logging.basicConfig(level=logging.INFO,
+                        format="%(levelname)s: %(message)s")
+
+    model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
+    input_text = "What's in this image?"
+    input_image = "path/to/image"
+
+    try:
+        session = boto3.Session(profile_name="AdministratorAccess-057286248359")
+        bedrock_client = session.client(service_name="bedrock-runtime")
+
+        response = generate_conversation(
+            bedrock_client, model_id, input_text, input_image)
+
+        output_message = response['output']['message']
+
+        print(f"Role: {output_message['role']}")
+
+        for content in output_message['content']:
+            print(f"Text: {content['text']}")
+
+        token_usage = response['usage']
+        print(f"Input tokens:  {token_usage['inputTokens']}")
+        print(f"Output tokens:  {token_usage['outputTokens']}")
+        print(f"Total tokens:  {token_usage['totalTokens']}")
+        print(f"Stop reason: {response['stopReason']}")
+
+    except ClientError as err:
+        message = err.response['Error']['Message']
+        logger.error("A client error occurred: %s", message)
+        print(f"A client error occured: {message}")
+
+    else:
+        print(
+            f"Finished generating text with model {model_id}.")
+
+
+if __name__ == "__main__":
+    main()
